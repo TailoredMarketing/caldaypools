6c92fce0d43f0e23d54316fbbe05177e0efa9045
@dantaylorseo <dan@tailored.im>
2015-08-19 20:24:26 +0100

    theme updates

--- wp-content/themes/calday_theme/index.php
+++ wp-content/themes/calday_theme/index.php
@@ -3,7 +3,15 @@
 	<div class="row">
         <section id="content" class="col-md-8">
             <h1><span><?php if( is_home() && get_option( 'page_for_posts' ) ) echo get_the_title( get_option( 'page_for_posts' ) ); ?></span></h1>
-            <?php the_content(); ?>
+            <?php while( have_posts() ) :
+					the_post();
+			?>
+            	<article>
+                	<h2><?php the_title(); ?></h2>
+                    <?php the_excerpt(); ?>
+                </article>
+            <?php endwhile; ?>
+					
         </section>
         <?php get_sidebar(); ?>
     </div>  
