0a03f612f8dd78b58c31a2237d9f18efbab0c2d0
@dantaylorseo <dan@tailored.im>
2015-08-19 20:25:08 +0100

    theme updates

--- wp-content/themes/calday_theme/index.php
+++ wp-content/themes/calday_theme/index.php
@@ -7,7 +7,7 @@
 					the_post();
 			?>
             	<article>
-                	<h2><?php the_title(); ?></h2>
+                	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <?php the_excerpt(); ?>
                 </article>
             <?php endwhile; ?>
